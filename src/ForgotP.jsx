7bacd740aa7af2ad3648358812f0f60cfb6a015c
Shalaka <shalakaw17@gmail.com>
2024-03-03 22:52:28 +0530

    Added Login & ForgotP

---
+++ src/ForgotP.jsx
@@ -0,0 +1,45 @@
+
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Checkbox, IconButton } from '@mui/material';
+import { pink } from '@mui/material/colors'
+import React from 'react'
+
+export default function ForgotP() {
+  return (
+    <> 
+<div className='bg-[#6092CC] flex justify-center items-center h-screen' >
+        <div className='w-96 p-6 bg-white rounded-lg h-100  '>
+        <div className=' text-lg font-medium '>
+            FORGOT PASSWORD
+        </div>
+        <div className='mt-3'>
+            <label htmlFor="Email" className='block mb-1 ' > Email </label>
+            <input type="email" id="email" className='rounded border-2 w-full px-2 py-1 focus:border-gray-600' />
+        </div>
+
+        <div className='mt-3'>
+            <label htmlFor="otp" className='block mb-1 ' > OTP </label>
+            <input type="text" id="otp" placeholder="Enter OTP recieved on registered Email" className='text-sm rounded border-2 w-full px-2 py-1 focus:border-gray-600' />
+        </div>
+        
+        <div className='mt-3'>
+            <button className='drop-shadow-lg mt-1 p-2 bg-pink-500 text-white w-full rounded-xl border-pink-700'>
+                VALIDATE
+            </button>
+        </div>
+       
+        <hr className='mt-2 mb-4 border-1'></hr>
+
+        <div className='mt-1 text-center '>
+            Go Back. 
+             <button className='pl-1 underline'> LOGIN
+              </button>
+        </div>
+        </div>
+    </div>
+
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import LogIn from './LogIn.jsx'
 import './index.css'
+import ForgotP from './ForgotP.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <ForgotP />
   </React.StrictMode>,
 )
+
