0e5237a7a3ae5319e718e5df901665d11923af6c
Shashikant Ramkaran Tripathi <cjshashikanttripathi@gamil.com>
2024-03-03 23:15:10 +0530

    shalaka login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
 import './index.css'
+import Singup from './Singup.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Singup />
   </React.StrictMode>,
 )
